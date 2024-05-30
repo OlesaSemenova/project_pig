800c363eedc5febd1e9bfef563a7111c7e974132
Olesa Semenova <semenovmaolesa@gmail.com>
2024-05-30 14:07:02 +0300

    Объявление переменных для начала разработки

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+//Element selection
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+// Game initial conditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+
+diceElement.classList.add('hidden');
