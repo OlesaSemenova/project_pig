6c1e0bda1016f383c76ddb8152645effb9f139a5
Olesa Semenova <semenovmaolesa@gmail.com>
2024-06-28 15:37:26 +0300

    Функционал смены игрока

--- script.js
+++ script.js
@@ -9,13 +9,17 @@ const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const plater0Element = document.querySelector('.player--0');
+const plater1Element = document.querySelector('.player--1');
 
 // Game initial conditions
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
+const totalScores = [0, 0];
 let currentScore = 0;
+let actinePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
   // 1. Generate a random number
@@ -28,7 +32,19 @@ btnRoll.addEventListener('click', function () {
   // 3. If the number is 1, switch to the next player, if not - add number to the current score
   if (diceNumber !== 1) {
     currentScore += diceNumber;
-    current0Element.textContent = currentScore; //Change later
+    document.getElementById(`current--${actinePlayer}`).textContent =
+      currentScore;
   } else {
+    currentScore = 0;
+    document.getElementById(`current--${actinePlayer}`).textContent =
+      currentScore;
+    actinePlayer = actinePlayer === 0 ? 1 : 0;
+    plater0Element.classList.toggle('player--active');
+    plater1Element.classList.toggle('player--active');
   }
+});
+
+btnHold.addEventListener('click', function () {
+  //1. Add current scort to active player total score
+  //2. If total score of active player >= 100? active player won, if not - switch active player
 });
